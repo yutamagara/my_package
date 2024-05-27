fca38bcf1cbeda70ea74cc0ac833f7b1c1091dd4
yutamagara <119424515+yutamagara@users.noreply.github.com>
2024-05-27 14:55:47 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,29 @@
+# setup.py
 from setuptools import setup, find_packages
 
 setup(
-    name='my_package',
-    version='0.1',
+    name='audio_searcher',
+    version='0.1.3',
     packages=find_packages(),
     install_requires=[
-        'pandas',
+        'SpeechRecognition',
+        'pydub'
     ],
     entry_points={
         'console_scripts': [
-            'my_package=my_package.my_module:main',
+            'audio-searcher=audio_searcher.audio_searcher:main',
         ],
     },
+    author='yutamagara',
+    author_email='s2222033@stu.musashino-u.ac.jp',
+    description='A tool to search for keywords in audio files',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yutamagara/mypackage',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
 )
